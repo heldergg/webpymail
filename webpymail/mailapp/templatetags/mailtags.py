472cf6a005e7bccf12debb6d9c45c9d378305d0d
Helder Guerreiro <helder@tretas.org>
2016-08-03 22:14:39 +0100

    Show TEXT/HTML message parts with tag show_part
    The html is sanitized using the bleach python library.
    Because bleach also has a tool for identifying and creating
    links (bleach.linkify), we're using it to process TEXT/PLAIN
    message parts.

--- webpymail/mailapp/templatetags/mailtags.py
+++ webpymail/mailapp/templatetags/mailtags.py
@@ -29,10 +29,37 @@ from django.utils.html import escape
 
 register = template.Library()
 
+import bleach
 import re, textwrap
 
-# TODO: This regex needs some tweeking to the query part:
-html_url_re = re.compile(r"(https?://(?:(?:(?:(?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]|[a-zA-Z0-9])\.)*(?:[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9]|[a-zA-Z]))|(?:[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))(?::[0-9]+)?)(?:/(?:(?:(?:[a-zA-Z]|[0-9]|[$\-_.+]|[!*'(),])|(?:%[0-9A-Fa-f][0-9A-Fa-f]))|[;:@&=])*(?:/(?:(?:(?:[a-zA-Z]|[0-9]|[$\-_.+]|[!*'(),])|(?:%[0-9A-Fa-f][0-9A-Fa-f]))|[;:@&=])*)*(?:\?(?:(?:(?:[a-zA-Z]|[0-9]|[$\-_.+]|[!*'(),])|(?:%[0-9A-Fa-f][0-9A-Fa-f]))|[;:@&=])*)?)?)")
+# Bleach configuration
+TAGS = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'br', 'code',
+        'em', 'em', 'h1', 'h2', 'h3', 'h4' 'hr', 'i', 'li', 'ol', 'p',
+        'strong', 'style', 'ul', 'div', 'table', 'th', 'td', 'tr',
+        'div', 'span', 'img']
+STYLES = ['azimuth', 'background-color', 'border-bottom-color',
+          'border-collapse', 'border-color', 'border-left-color',
+          'border-right-color', 'border-top-color', 'clear',
+          'color', 'cursor', 'direction', 'display', 'elevation',
+          'float', 'font', 'font-family', 'font-size', 'font-style',
+          'font-variant', 'font-weight', 'height', 'letter-spacing',
+          'line-height', 'overflow', 'pause', 'pause-after',
+          'pause-before', 'pitch', 'pitch-range', 'richness',
+          'speak', 'speak-header', 'speak-numeral', 'speak-punctuation',
+          'speech-rate', 'stress', 'text-align', 'text-decoration',
+          'text-indent', 'unicode-bidi', 'vertical-align',
+          'voice-family', 'volume', 'white-space', 'width']
+ATTRS = {'*': ['class', 'id', 'style', ],
+         'a': ['href', 'title'],
+         'abbr': ['title'],
+         'acronym': ['title'],
+         'img': ['alt','title', 'width', 'height'],
+         'table': ['width', 'align', 'cellpadding', 'cellspacing',
+                   'border'],
+         'td': ['width', 'valign'],
+         'th': ['width', 'valign'],
+         }
+PROTOCOLS = ['http', 'https', 'mailto'] # Not available in bleach 1.4.3
 
 # Tag to retrieve a message part from the server:
 
@@ -44,12 +71,7 @@ def do_show_part(parser, token):
     except ValueError:
         raise template.TemplateSyntaxError("%r tag requires two args: message, part" \
             % token.contents.split()[0])
-
     return PartTextNode(message, part)
-
-def make_links( match ):
-    url = match.groups()[0]
-    return '<a href="%s">\n%s</a>' % (url, url)
 
 def wrap_lines(text, colnum = 72):
     ln_list = text.split('\n')
@@ -58,19 +80,38 @@ def wrap_lines(text, colnum = 72):
         if len(ln) > colnum:
             ln = textwrap.fill(ln, colnum)
         new_list.append(ln)
-
     return '\n'.join(new_list)
 
 class PartTextNode(template.Node):
-    def __init__(self, message, part ):
+    def __init__(self, message, part, external_images=True ):
         self.message = message
         self.part = part
+        self.external_images = external_images
+
+    def sanitize_text(self, text):
+        text = escape(text)
+        # text = html_url_re.sub(make_links, text)
+        text = bleach.linkify(text)
+        text = wrap_lines( text, 80 )
+        return text
+
+    def sanitize_html(self, text):
+        if self.external_images:
+            ATTRS['img'].append('src')
+        text = bleach.clean(text,
+                tags=TAGS,
+                attributes=ATTRS,
+                styles=STYLES,
+                strip=True)
+        return text
 
     def render(self, context):
         message =  resolve_variable(self.message, context)
         part = resolve_variable(self.part, context)
-        text = escape(message.part( part ))
-        text = html_url_re.sub( make_links, text)
-        if part.media == 'TEXT' and part.media_subtype == 'PLAIN':
-            text = wrap_lines( text, 80 )
+        text = message.part(part)
+        if part.is_plain():
+            text = self.sanitize_text(text)
+        elif part.is_html():
+            text = self.sanitize_html(text)
+        print([text])
         return text
