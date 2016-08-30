3bf1ee3bff16166044a4a7a47b145c0308d0a9e3
Helder Guerreiro <helder@tretas.org>
2016-08-30 20:59:00 +0100

    Code cleanup
    Remade the message list refresh code. Removed the message
    paginator and the message sorte to their own files.

--- hlimap/imapmessage.py
+++ hlimap/imapmessage.py
@@ -104,8 +104,14 @@ Please note the THREAD command response is in the form:
 '''
 
 # Imports
-import quopri, base64
+import base64
+import quopri
+
 from imaplib2.parsefetch import Single
+
+from .message_threader import Threader
+from .message_sorter import Sorter, SortProgError
+from .message_paginator import Paginator, PaginatorError
 
 # Utils
 
@@ -120,10 +126,9 @@ def flaten_nested(nested_list):
             yield item
 
 def threaded_tree(nested_list, base_level = 0, parent = None):
-    '''Analyses the tree
+    '''Analyses the tree, returns child depth (level)
     '''
     level = base_level
-
     for item in nested_list:
         if type(item) in (list, tuple):
             for sub_item in threaded_tree(item, level, parent ):
@@ -135,10 +140,7 @@ def threaded_tree(nested_list, base_level = 0, parent = None):
 
 # Exceptions:
 
-class SortProgError(Exception): pass
-class PaginatorError(Exception): pass
 class MessageNotFound(Exception): pass
-class NotImplementedYet(Exception): pass
 
 # Constants:
 
@@ -155,133 +157,6 @@ ANSWERED = r'\Answered'
 FLAGGED = r'\Flagged'
 DRAFT = r'\Draft'
 RECENT = r'\Recent'
-
-class Paginator(object):
-    def __init__(self, msg_list):
-        self.msg_list = msg_list
-        # self.msg_per_page = -1 => ALL MESSAGES
-        self.msg_per_page = 50
-        self.__page = 1
-
-    def _get_max_page(self):
-        if self.msg_per_page == -1:
-            return 1
-        if self.msg_list.number_messages % self.msg_per_page:
-            return 1 + self.msg_list.number_messages // self.msg_per_page
-        else:
-            return self.msg_list.number_messages // self.msg_per_page
-    max_page = property(_get_max_page)
-
-    def _set_page(self, page):
-        if page < 1:
-            page = 1
-        elif page > self.max_page:
-            page = self.max_page
-        if self.__page != page:
-            self.refresh = True
-        self.__page = page
-
-    def _get_page(self):
-        if self.msg_per_page == -1:
-            return 1
-        return self.__page
-    current_page = property(_get_page, _set_page)
-
-    def has_next_page(self):
-        return self.current_page < self.max_page
-
-    def next(self):
-        if self.has_next_page():
-            return self.current_page + 1
-        else:
-            return 1
-
-    def has_previous_page(self):
-        return self.current_page > 1
-
-    def previous(self):
-        if self.has_previous_page():
-            return self.current_page - 1
-        else:
-            return self.max_page
-
-    def is_last(self):
-        return self.current_page == self.max_page
-
-    def is_not_last(self):
-        return self.current_page < self.max_page
-
-    def last(self):
-        return self.max_page
-
-    def is_first(self):
-        return self.current_page == 1
-
-    def is_not_first(self):
-        return self.current_page > 1
-
-class Threader:
-    '''Implements the client side REFERENCES threading algorithm defined in
-    RFC 5256 - https://tools.ietf.org/html/rfc5256
-    This is an adaptation of the threading algorithm by Jamie Zawinski which
-    was included in Netscape News and Mail 2.0 and 3.0:
-    https://www.jwz.org/doc/threading.html
-    '''
-    def __init__(self, message_list, message_dict):
-        self.message_list = message_list
-        self.message_dict = message_dict
-    def run(self):
-        return self.message_list
-
-class Sorter:
-    '''This class provides the comparison function for sorting messages
-    according to the provided sort program.
-    '''
-    def __init__(self, message_list, message_dict, sort_program, threaded):
-        '''
-        @message_list - list to be sorted
-        @message_dict - dict containing information about the messages in the
-          form { MSG_UID: { msg info }, ... }
-        @sort_program - tipple containing the sort program
-        '''
-        self.message_list = message_list
-        self.message_dict = message_dict
-        self.sort_program = sort_program
-        self.threaded     = threaded
-
-    def key_ARRIVAL(self, k):
-        return self.message_dict[k]['data'].internaldate
-
-    def key_CC(self, k):
-        return ', '.join( self.message_dict[k]['data'].envelope.cc_short() )
-
-    def key_FROM(self, k):
-        return ', '.join( self.message_dict[k]['data'].envelope.from_short() )
-
-    def key_DATE(self, k):
-        return self.message_dict[k]['data'].envelope['env_date']
-
-    def key_SIZE(self, k):
-        return self.message_dict[k]['data'].size
-
-    def key_SUBJECT(self, k):
-        return self.message_dict[k]['data'].envelope['env_subject']
-
-    def key_TO(self, k):
-        return ', '.join( self.message_dict[k]['data'].envelope.to_short() )
-
-    def run(self):
-        '''Read the sort program and executes it
-        '''
-        if not self.threaded:
-            for keyword in reversed(self.sort_program):
-                reverse = False
-                if keyword[0] == '-':
-                    reverse = True
-                    keyword = keyword[1:]
-                key_meth = getattr(self, 'key_%s' % keyword )
-                self.message_list.sort(key=key_meth,reverse=reverse)
-        return self.message_list
 
 class MessageList(object):
     def __init__(self, server, folder, threaded=False):
@@ -319,8 +194,8 @@ class MessageList(object):
         self.paginator = Paginator(self)
 
         # REMOVE THIS! This is to test the client side sorting and threading
-        self.show_style = THREADED
-        self.search_capability = [UNSORTED, THREADED]
+        self.show_style = SORTED
+        self.search_capability = [UNSORTED]
 
     # Sort program:
     def sort_string(self):
@@ -391,7 +266,7 @@ class MessageList(object):
             # We have the SORT extension on the server:
             message_list = self._imap.sort(self.sort_string(),
                 'utf-8', self.search_expression)
-            flat_message_list = message_list
+            flat_message_list = message_list[:]
         else:
             # Just get the list.
             message_list = list(self._imap.search(self.search_expression))
@@ -455,9 +330,8 @@ class MessageList(object):
         +--------------+--------+--------+------+--------+--------+--------+
         |              |      1 |      1 |    1 |      1 |      1 |      1 |
         |              |      2 |      2 |    5 |      5 |      2 |      2 |
-        |              |      4 |      3 |    2 |      2 |      4 |      4 |
-        |              |      5 |      4 |      |      3 |      5 |      5 |
-        |              |        |      5 |      |        |        |        |
+        |              |      4 |      3 |    2 |      2 |      4 |      5 |
+        |              |      5 |      5 |      |      3 |      5 |        |
         +--------------+--------+--------+------+--------+--------+--------+
 
         Steps:
@@ -493,17 +367,19 @@ class MessageList(object):
         # Client side threading
         if self.show_style==THREADED and THREADED not in self.search_capability:
             message_list = Threader(message_list, message_dict).run()
+            flat_message_list = list(flaten_nested(message_list))
         # Client side sorting
-        if SORTED not in self.search_capability:
+        if SORTED not in self.search_capability and self.show_style==SORTED:
             message_list = Sorter(
                     message_list,
                     message_dict,
-                    self.sort_program,
-                    self.show_style == THREADED).run()
-        # Update the message dict with the level information of about the
-        # thread level of each message
+                    self.sort_program).run()
+            flat_message_list = list(message_list)
+        # Update the message dict with the level information of the
+        # thread level of each message and each message children
         if self.show_style==THREADED:
             message_dict = self.update_message_dict(message_list, message_dict)
+            # Sort the threads according to the defined program
         # House keeping
         self.message_dict = message_dict
         self.flat_message_list = flat_message_list
@@ -542,20 +418,43 @@ class MessageList(object):
     def __repr__(self):
         return '<MessageList instance in folder "%s">' % (self.folder.name)
 
-
 class Message(object):
     def __init__(self, server, folder, msg_info):
         self.server = server
         self._imap = server._imap
         self.folder = folder
+        # Problem: msg_info carries lots of information that can vary quite a
+        # bit. We could query this information from within this class, but then
+        # we would have to query the IMAP server once for each message this is
+        # not efficient.
+        # It seems that collecting this info is arbitrary and will force the
+        # user to alter this class if new information is necessary.
+        # This should be open.
+        # TODO: Create a manager to collect this information on demand or
+        # to store it if it is available in msg_info when creating a new
+        # Message instance.
         self.envelope = msg_info['ENVELOPE']
         self.size = msg_info['RFC822.SIZE']
         self.uid = msg_info['UID']
         self.id = msg_info['ID']
-        self.get_flags( msg_info['FLAGS'] )
+        self.get_flags(msg_info['FLAGS'])
         self.internaldate = msg_info['INTERNALDATE']
+        self.references = self.get_references(msg_info)
         self.level = 0 # Thread level
         self.__bodystructure = None
+
+    # References
+    def get_references(self, msg_info):
+        ref_list = []
+        if 'BODY[HEADER.FIELDS (REFERENCES)]' in msg_info:
+            ref_list = msg_info['BODY[HEADER.FIELDS (REFERENCES)]']
+            ref_list = ref_list.split('References:')
+            if len(ref_list)<2:
+                return []
+            return [ref.strip(' \r\n\t')
+                for ref in msg_info['BODY[HEADER.FIELDS (REFERENCES)]'].split(
+                    'References:')[1].split() if ref.strip(' \r\n\t')]
+        return []
 
     # Fetch messages
     def get_bodystructure(self):
